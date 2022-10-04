17381cf379ad17b933ea16eb3ec13fa6e257e40c
Alec Perry <alexander.c.perry@gmail.com>
2022-10-04 14:37:43 -0600

    began implementing try & catch blocks to handle exceptions - but still need to implement throw blocks

--- Challenge15/main.cpp
+++ Challenge15/main.cpp
@@ -9,69 +9,105 @@
 
 using namespace std;
 
+//Exception Classes
+//-----------------------------------------------------------------
+class NegativeBalanceException : public std::exception {
+public:
+    NegativeBalanceException() noexcept = default;
+    ~NegativeBalanceException() = default;
+    virtual const char * what() const noexcept{
+        return "Account with a negative balance cannot be created - please enter a value greater than or equal to zero";
+    }
+};
+
+class OverdraftException : public std::exception {
+public:
+    OverdraftException() noexcept = default;
+    ~OverdraftException() = default;
+    virtual const char * what() const noexcept{
+        return "Cannot withdraw more money than is account - please enter a value less than or equal to account balance";
+    }
+};
+
 int main() {
     cout.precision(2);
     cout << fixed;
     
-    //Savings Accounts
-    //-----------------------------------------------------------------
-    Account * sa1 = new Savings_Account {};
-    Account * sa2 = new Savings_Account {"Superman"};
-    Account * sa3 = new Savings_Account {"Batman", 2000};
-    Account * sa4 = new Savings_Account {"Wonderwoman", 5000, 5.0};
-    
-    vector <Account *> savings_accounts;
-    savings_accounts.push_back(sa1);
-    savings_accounts.push_back(sa2);
-    savings_accounts.push_back(sa3);
-    savings_accounts.push_back(sa4);
-    
-    deposit(savings_accounts, 1000.00);
-    withdraw(savings_accounts,2000.00);
-    display(savings_accounts);
-    
-    for (auto acct_ptr : savings_accounts)
-        delete acct_ptr;
-    
-    // Checking Accounts
-    //-----------------------------------------------------------------
-    Account * ca1 = new Checking_Account {};
-    Account * ca2 = new Checking_Account {"Inky"};
-    Account * ca3 = new Checking_Account {"Pinky", 1000};
-    Account * ca4 = new Checking_Account {"Winky", 2000, 1.00};
-    
-    vector <Account *> checking_accounts;
-    checking_accounts.push_back(ca1);
-    checking_accounts.push_back(ca2);
-    checking_accounts.push_back(ca3);
-    checking_accounts.push_back(ca4);
+//Application Testing
+//-----------------------------------------------------------------
 
-    deposit(checking_accounts, 100);
-    withdraw(checking_accounts, 1100);
-    display(checking_accounts);
-    
-    for (auto acct_ptr : checking_accounts)
-        delete acct_ptr;
- 
-    // Trust Accounts
+    //Exception Testing
     //-----------------------------------------------------------------
-    Account * ta1 = new Trust_Account {};
-    Account * ta2 = new Trust_Account {"Grumpy"};
-    Account * ta3 = new Trust_Account {"Sleepy", 2000};
-    Account * ta4 = new Trust_Account {"Mopey", 4000, 5};
+    //try block tests are 1.) negative balance initiation, and 2.) overdraft
+    //negative balance initialization
+    try {
+        Account * negative_savings = new Savings_Account {"Scooby", -1, 1.0};
+    }
     
-    vector <Account *> trust_accounts;
-    trust_accounts.push_back(ta1);
-    trust_accounts.push_back(ta2);
-    trust_accounts.push_back(ta3);
-    trust_accounts.push_back(ta4);
+    //catch blocks
+    catch (const NegativeBalanceException &ex) {
+        std::cerr << ex.what() << std::endl;
+    }
     
-    deposit(trust_accounts, 200);
-    deposit(trust_accounts, 2000);
-    withdraw(trust_accounts, 473);
-    withdraw(trust_accounts, 100);
-    withdraw(trust_accounts, 100);
-    withdraw(trust_accounts, 100);
+//    //Savings Accounts
+//    //-----------------------------------------------------------------
+//    Account * sa1 = new Savings_Account {};
+//    Account * sa2 = new Savings_Account {"Superman"};
+//    Account * sa3 = new Savings_Account {"Batman", 2000};
+//    Account * sa4 = new Savings_Account {"Wonderwoman", 5000, 5.0};
+//    
+//    vector <Account *> savings_accounts;
+//    savings_accounts.push_back(sa1);
+//    savings_accounts.push_back(sa2);
+//    savings_accounts.push_back(sa3);
+//    savings_accounts.push_back(sa4);
+//    
+//    deposit(savings_accounts, 1000.00);
+//    withdraw(savings_accounts,2000.00);
+//    display(savings_accounts);
+//    
+//    for (auto acct_ptr : savings_accounts)
+//        delete acct_ptr;
+//    
+//    // Checking Accounts
+//    //-----------------------------------------------------------------
+//    Account * ca1 = new Checking_Account {};
+//    Account * ca2 = new Checking_Account {"Inky"};
+//    Account * ca3 = new Checking_Account {"Pinky", 1000};
+//    Account * ca4 = new Checking_Account {"Winky", 2000, 1.00};
+//    
+//    vector <Account *> checking_accounts;
+//    checking_accounts.push_back(ca1);
+//    checking_accounts.push_back(ca2);
+//    checking_accounts.push_back(ca3);
+//    checking_accounts.push_back(ca4);
+//
+//    deposit(checking_accounts, 100);
+//    withdraw(checking_accounts, 1100);
+//    display(checking_accounts);
+//    
+//    for (auto acct_ptr : checking_accounts)
+//        delete acct_ptr;
+// 
+//    // Trust Accounts
+//    //-----------------------------------------------------------------
+//    Account * ta1 = new Trust_Account {};
+//    Account * ta2 = new Trust_Account {"Grumpy"};
+//    Account * ta3 = new Trust_Account {"Sleepy", 2000};
+//    Account * ta4 = new Trust_Account {"Mopey", 4000, 5};
+//    
+//    vector <Account *> trust_accounts;
+//    trust_accounts.push_back(ta1);
+//    trust_accounts.push_back(ta2);
+//    trust_accounts.push_back(ta3);
+//    trust_accounts.push_back(ta4);
+//    
+//    deposit(trust_accounts, 200);
+//    deposit(trust_accounts, 2000);
+//    withdraw(trust_accounts, 473);
+//    withdraw(trust_accounts, 100);
+//    withdraw(trust_accounts, 100);
+//    withdraw(trust_accounts, 100);
     
     return 0;
 }
